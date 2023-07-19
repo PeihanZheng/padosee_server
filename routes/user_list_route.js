4e8d11f50006b5b88120e8ce360c2390ff4b5e0c
Chittebabu <chittebabu@graymatics.com>
2023-07-19 16:49:30 +0800

    Update user_list_route.js
    Added updateUserPassword route.

--- routes/user_list_route.js
+++ routes/user_list_route.js
@@ -1,10 +1,11 @@
-const { createUser, getUsers, getUserById, updateUser, deleteUser, login } = require("../controllers/user_list_controller.js");
+const { createUser, getUsers, getUserById, updateUser, updateUserPassword, deleteUser, login } = require("../controllers/user_list_controller.js");
 const router = require("express").Router();
 
 router.post("/", createUser);
 router.get("/", getUsers);
 router.get("/:id", getUserById);
 router.put("/", updateUser);
+router.put("/update-password/:id", updateUserPassword);
 router.delete("/:id", deleteUser);
 router.post("/login", login);
 
