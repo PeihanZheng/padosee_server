95ca55279918993b01dd7feb19ee83b5c0c293b9
Chittebabu <chittebabu@graymatics.com>
2023-08-10 23:16:37 +0800

    Update user_list_route.js
    Added route to send reset password link

--- routes/user_list_route.js
+++ routes/user_list_route.js
@@ -1,12 +1,17 @@
-const { createUser, getUsers, getUserById, updateUser, updateUserPassword, deleteUser, login } = require("../controllers/user_list_controller.js");
+// import methods
+const { createUser, getUsers, getUserById, updateUser, updateUserPassword, deleteUser, login, sendResetPasswordLink } = require("../controllers/user_list_controller.js");
+const auth = require("../auth/auth_middleware.js");
 const router = require("express").Router();
 
-router.post("/", createUser);
+// routers
 router.get("/", getUsers);
 router.get("/:id", getUserById);
-router.put("/", updateUser);
+router.post("/", createUser);
+router.put("/:id", auth, updateUser);
 router.put("/update-password/:id", updateUserPassword);
 router.delete("/:id", deleteUser);
 router.post("/login", login);
+router.post("/send-reset-password-link", sendResetPasswordLink);
 
+// export router
 module.exports = router;
